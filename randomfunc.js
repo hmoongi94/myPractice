04defe94fb0e0d3502fab5a34e1fab2f413500ab
hongmoongi <hmoongi94@gmail.com>
2023-10-08 22:47:46 +0900

    Math.floor와 Math.random으로 랜덤정수만 나오게하기

--- randomfunc.js
+++ randomfunc.js
@@ -4,3 +4,10 @@
 console.log((Math.random()*10)+1)
 
 console.log((Math.random()*(10-1))+1)
+
+// JavaScript Math.random() 자릿수 소숫점 삭제
+// Math.floor(2.5) -> 2 반환
+// 주의할 점 음수일 경우 Math.floor(-2.5) -> -2가 아닌 -3반환
+
+// 1~10까지 정수만 반환
+console.log(Math.floor((Math.random()*10)+1))
