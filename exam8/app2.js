bd46449e8637837e5754f1fa1b07fe8550518341
HongMoonGI <hmoongi94@gmail.com>
2023-10-16 17:11:59 +0900

    배열로 이미지 불러오기

--- exam8/app2.js
+++ exam8/app2.js
@@ -1,17 +1,21 @@
-const fs = require("fs")
+const fs = require("fs");
+const { request } = require("http");
 const path = require("path")
 
-let arr = [];
+let arrjpg = [];
 fs.readdir("./static/jpgimage",function(err, filelist){
   if(err){
     console.log("err")
   }
   pathparse = "";
   for(let i=0; i<filelist.length; i++){
-    arr[i]= filelist[i];
-    let pathparse = path.basename(arr[i], ".jpg")
+    arrjpg[i]= filelist[i];
+    let pathparse = path.basename(arrjpg[i], ".jpg")
     console.log(pathparse)
-
-
   }
+  if(request.url === `/${pathparse}`){
+    console.log(request.url);
+    fs.readFile(``)
+  }
+
 })
