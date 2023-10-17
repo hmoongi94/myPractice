588b7004b2bdfad60a60dd2ed74f3b42b554cb7e
HongMoonGI <hmoongi94@gmail.com>
2023-10-17 18:10:29 +0900

    배열 비교하기

--- arraycompare.js
+++ arraycompare.js
@@ -6,8 +6,9 @@ const arr3 = [1,2,3,4]
 const equals = (a,b) =>{
   console.log(JSON.stringify(a) === JSON.stringify(b)) 
 }
-
 // equals(arr3,arr2)
+
+////////////////////////////////////
 
 // 직접 만들어쓰기 array.prototype.every()
 // every(and), some(or)
@@ -21,3 +22,15 @@ console.log(equals2(arr1,arr2))
 // sort메서드 사용
 const arr4 = [2,4,1,3]
 // console.log(arr4.sort())
+
+///////////////////////////////////
+
+// for문 사용
+const arrayA = [];
+const arrayB = ["피카츄","라이츄","파이리"]
+// 절차방식
+for(let i=0; i<arrayB.length; i++){
+  arrayA[i]=arrayB[i]
+}
+// 선언방식
+const arrayC = arrayA.map(data=>data);
