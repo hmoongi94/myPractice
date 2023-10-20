65e56421717b4e8764c4cc3d8c80035876b6692b
HongMoonGI <hmoongi94@gmail.com>
2023-10-20 16:32:04 +0900

    readfile로 이미지,html파일 불러오기

--- serverPractice/serverPractice.js
+++ serverPractice/serverPractice.js
@@ -4,9 +4,10 @@ const port = process.env.PORT || 3000
 
 function serveStaticFile(res, path, contentType, responseCode = 200){
   fs.readFile(__dirname + path, (err,data)=>{
+    console.log(__dirname)
     if(err){
       res.writeHead(500, {'Content-Type': 'text/plain'})
-      return res.end('500- Internal Error')
+      return res.end('500-Internal Error')
     }
     res.writeHead(responseCode, {'Content-Type': contentType})
     res.end(data)
@@ -23,8 +24,8 @@ const server =http.createServer((req,res)=>{
     case '/about':
       serveStaticFile(res, '/public/about.html', 'text/html')
       break
-    case '/img/logo.png':
-      serveStaticFile(res, '/public/img/logo.png', 'image/png')
+    case '/img/logo3.png':
+      serveStaticFile(res, '/public/img/logo3.png', 'image/png')
       break
     default:
       serveStaticFile(res, 'public/404.html', 'text/html', 404)
