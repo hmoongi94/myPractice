0bab0a162da81e82b819b6dfbec8cccbdae00653
HongMoonGI <hmoongi94@gmail.com>
2023-10-20 12:37:05 +0900

    serveStaticFile 함수 생성

--- serverPractice/serverPractice.js
+++ serverPractice/serverPractice.js
@@ -1,6 +1,18 @@
+const { fstat } = require('fs')
 const http = require('http')
 const port = process.env.PORT || 3000
 const contentType = {"Content-Type": "text/plain"}
+
+function serveStaticFile(res, path, contenttype = contentType, responseCode = 200){
+  fs.readFile(__dirname + path, (err,data)=>{
+    if(err){
+      res.writeHead(500,contentType)
+      return res.end('500- Internal Error')
+    }
+    res.writeHead(responseCode, contenttype)
+    res.end(data)
+  })
+}
 
 const server =http.createServer((req,res)=>{
   // 쿼리스트링, 옵션인 마지막 슬래시를 없애고 소문자로 바꿔서 url을 정규화합니다.
