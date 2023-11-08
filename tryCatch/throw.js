0255d2768a29b062f1fe7aeb4d6f6e837c8d77f3
HongMoonGI <hmoongi94@gmail.com>
2023-11-08 15:00:12 +0900

    catch(error)는 관례적인 표현 굳이 error일 필요는없음

--- tryCatch/throw.js
+++ tryCatch/throw.js
@@ -1,10 +1,31 @@
-const throwfunction = function(){
+// function throwFunction(){
+//   console.log("throw로 인해 예외처리 된 경우입니다.")
+// }
+
+
+// function divide(a, b) {
+//   if (b === 0) {
+//     throw throwFunction()
+//   }
+//   return a / b;
+// }
+
+// try {
+//   const result = divide(10, 0);
+//   console.log('나눈 결과:', result);
+// } catch (throwFunction) {
+//   throwFunction();
+// }
+
+// console.log(10/0)
+
+function throwFunction(){
   console.log("throw로 인해 예외처리 된 경우입니다.")
 }
 
 function divide(a, b) {
   if (b === 0) {
-    throw throwFunction()
+    throw throwFunction
   }
   return a / b;
 }
@@ -12,6 +33,6 @@ function divide(a, b) {
 try {
   const result = divide(10, 0);
   console.log('나눈 결과:', result);
-} catch (error) {
-  throwFunction();
+} catch(execption) {
+  throwFunction(); // 예외 처리 후 함수 호출
 }
