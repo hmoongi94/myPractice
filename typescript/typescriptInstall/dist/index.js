5512c3fc4ac7853a75a85f2fa9edd0cadd0fd867
HongMoonGI <hmoongi94@gmail.com>
2023-11-14 16:46:24 +0900

    타입: any 개념 알아보기

--- typescript/typescriptInstall/dist/index.js
+++ typescript/typescriptInstall/dist/index.js
@@ -1,9 +1,19 @@
 "use strict";
-let a = 1 + 2;
-let b = a + 3;
-let c = {
-    apple: a,
-    banana: b
-};
-let d = c.apple * 4;
+// let a = 1+2
+// let b = a+3
+// let c ={
+//   apple: a,
+//   banana: b
+// }
+// let d = c.apple*4
+// console.log(d)
+function squareof(n) {
+    console.log(n * n);
+}
+squareof(2);
+squareof('z');
+let a = 666;
+let b = ['danger'];
+let c = a + b;
+console.log(c);
 //# sourceMappingURL=index.js.map
--- typescript/typescriptInstall/src/index.ts
+++ typescript/typescriptInstall/src/index.ts
@@ -1,7 +1,20 @@
-let a = 1+2
-let b = a+3
-let c ={
-  apple: a,
-  banana: b
+// let a = 1+2
+// let b = a+3
+// let c ={
+//   apple: a,
+//   banana: b
+// }
+// let d = c.apple*4
+// console.log(d)
+
+function squareof(n:number){
+  console.log(n*n)
 }
-let d = c.apple*4
+
+squareof(2)
+squareof('z')
+
+let a: any =666
+let b: any =['danger']
+let c = a+b
+console.log(c)
