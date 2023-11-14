74c9e35d3a86424c3bfe1149495bb6cf42015b82
HongMoonGI <hmoongi94@gmail.com>
2023-11-14 16:53:05 +0900

    데이터타입 unknown 이해하기

--- typescript/typescriptInstall/dist/index.js
+++ typescript/typescriptInstall/dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+// * typescript 맛보기
 // let a = 1+2
 // let b = a+3
 // let c ={
@@ -7,13 +8,25 @@
 // }
 // let d = c.apple*4
 // console.log(d)
-function squareof(n) {
-    console.log(n * n);
+// * any
+// function squareof(n:number){
+//   console.log(n*n)
+// }
+// squareof(2)
+// squareof('z')
+// let a: any =666
+// let b: any =['danger']
+// let c = a+b
+// console.log(c)
+// * unknown
+let a = 30;
+let b = a === 123;
+let c = a + 10;
+if (typeof a === 'number') {
+    let d = a + 10;
 }
-squareof(2);
-squareof('z');
-let a = 666;
-let b = ['danger'];
-let c = a + b;
+console.log(a);
+console.log(b);
 console.log(c);
+console.log(d);
 //# sourceMappingURL=index.js.map
--- typescript/typescriptInstall/src/index.ts
+++ typescript/typescriptInstall/src/index.ts
@@ -1,3 +1,4 @@
+// * typescript 맛보기
 // let a = 1+2
 // let b = a+3
 // let c ={
@@ -7,14 +8,27 @@
 // let d = c.apple*4
 // console.log(d)
 
-function squareof(n:number){
-  console.log(n*n)
-}
+// * any
+// function squareof(n:number){
+//   console.log(n*n)
+// }
 
-squareof(2)
-squareof('z')
+// squareof(2)
+// squareof('z')
 
-let a: any =666
-let b: any =['danger']
-let c = a+b
-console.log(c)
+// let a: any =666
+// let b: any =['danger']
+// let c = a+b
+// console.log(c)
+
+// * unknown
+// let a: unknown = 30
+// let b = a === 123
+// let c = a +10
+// if(typeof a === 'number'){
+//   let d = a + 10
+// }
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
