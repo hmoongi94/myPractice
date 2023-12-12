9a07d18eee26a4de2ed68465d1fab24c863a754d
HongMoonGI <hmoongi94@gmail.com>
2023-12-12 19:46:03 +0900

    feat: 데이터 연결해보기

--- react/html-training3/js/index.js
+++ react/html-training3/js/index.js
@@ -3,37 +3,16 @@ import { render } from "./util/render.js";
 import { componentParent } from "./component/componentParent.js";
 import { componentLeftSide } from "./component/componentLeftSide.js";
 
+import { data } from "./data/data.js";
+
 export function load() {
   document.addEventListener('DOMContentLoaded', function () {
-
-    function component(stateData) {
-      const menuItems = [];
-      for (let i = 0; i < stateData.length; i++) {
-        const item = stateData[i]
-        const menuItem = createElement('li', {}, createElement('a', { href: item.hash }, item.text))
-        menuItems.push(menuItem)
-      }
-
-      const menu = createElement('ul', {}, ...menuItems)
-      const content = createElement('div', {}, 'Hello React')
-
-      return createElement('div', {}, menu, content)
-    }
-
-    const stateData = [
-      { hash: '#home', text: 'Home' },
-      { hash: '#about', text: 'About' },
-      { hash: '#services', text: 'Services' },
-      { hash: '#portfolio', text: 'portfolio' },
-      { hash: '#contact', text: 'Contact' }
-    ]
 
     const container = document.getElementById('root')
     // container.classList.add('root')
     console.dir(container)
 
-    container.appendChild(render(component(stateData)))
-    container.appendChild(render(componentParent()))
+    container.appendChild(render(componentParent(data.article1)))
     
   })
 }
