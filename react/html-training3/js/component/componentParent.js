92fa1d43f87ecb0909aa5908317030e12d4ede2a
HongMoonGI <hmoongi94@gmail.com>
2023-12-12 19:51:11 +0900

    feat: 스타일 데이터 불러오기

--- react/html-training3/js/component/componentParent.js
+++ react/html-training3/js/component/componentParent.js
@@ -1,7 +1,8 @@
 import { createElement } from "../util/createElement.js";
+import { styleData } from "../data/styleData.js";
 
 export function componentParent(data){
-  let Parent =  createElement('div',{style:'display:flex; background-color:grey'},createElement('div',{style:'background-color:white; width:434px; height:348px; top:162px; left:113px; font-weight:400; font-size: 16px; line-height:19.36px; font: color #000; margin: 30px'}, data),createElement('div',{style:'width: 1200px; height:348px; margin:30px'},'자식2'))
+  let Parent =  createElement('div', styleData.parentAttribute ,createElement('div',styleData.leftChildAttribute, data),createElement('div',styleData.rightChildAttribute,'자식2'))
   
   return Parent
 }
---
+++ react/html-training3/js/data/styleData.js
@@ -0,0 +1,5 @@
+export const styleData = {
+  parentAttribute: {style:'display:flex; background-color:grey'},
+  leftChildAttribute: {style:'background-color:white; width:434px; height:348px; top:162px; left:113px; font-weight:400; font-size: 16px; line-height:19.36px; font: color #000; margin: 30px'},
+  rightChildAttribute: {style:'width: 1200px; height:348px; margin:30px'}
+}
