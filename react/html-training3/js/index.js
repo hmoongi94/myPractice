08700801dcfefcce3971059d0c3cd7eb0cd78b5e
HongMoonGI <hmoongi94@gmail.com>
2023-12-12 17:39:41 +0900

    feat: 주석 제거

--- react/html-training3/js/index.js
+++ react/html-training3/js/index.js
@@ -10,14 +10,11 @@ export function load() {
         const item = stateData[i]
         const menuItem = createElement('li', {}, createElement('a', { href: item.hash }, item.text))
         menuItems.push(menuItem)
-        // menuItems라는 배열에 sateData의 데이터값을 사용해 {type:'li', property:{}, children:{type:'a', property:{href:#data}, children: [data] }} 값인 
       }
-      // console.log(menuItems)
-      // console.log(...menuItems)
+
       const menu = createElement('ul', {}, ...menuItems)
       const content = createElement('div', {}, 'Hello React')
-      // console.log(createElement('div', {}, menu, content))
-      // children: [{...},{...}]
+
       return createElement('div', {}, menu, content)
     }
 
