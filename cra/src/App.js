9276a68ae138e610c51aeb54fae868e49ef07d96
HongMoonGI <hmoongi94@gmail.com>
2023-12-19 10:54:40 +0900

    feat: 데이터 표시해보기

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,19 +1,22 @@
-// import logo from './logo.svg';
-import './App.css';
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
 
-function MyButton() {
+export default function Profile() {
   return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
-
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
   );
 }
