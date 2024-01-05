964c7e97777a0d1ceb24c0f32a86cb4bb491008b
HongMoonGI <hmoongi94@gmail.com>
2024-01-05 16:02:39 +0900

    feat: 객체로 메서드를 사용해서 데이터베이스 연결해보기

--- relative-database/server.js
+++ relative-database/server.js
@@ -6,11 +6,15 @@ const port = 3001;
 
 const orders = [
   { OrderID: 1, CustomerID: 1, OrderDate: '2024-01-05' },
+  { OrderID: 2, CustomerID: 5, OrderDate: '2024-01-05' },
+  { OrderID: 3, CustomerID: 6, OrderDate: '2024-01-05' },
   // ... 다른 주문 데이터들
 ];
 
 const orderDetails = [
   { DetailID: 1, OrderID: 1, ProductID: 201, Quantity: 2 },
+  { DetailID: 3, OrderID: 2, ProductID: 203, Quantity: 5 },
+  { DetailID: 5, OrderID: 3, ProductID: 205, Quantity: 3 },
   // ... 다른 주문 상세 데이터들
 ];
 
@@ -26,9 +30,16 @@ app.get('/', (req, res) => {
 app.get('/order-details/:orderId', (req, res) => {
   const orderId = parseInt(req.params.orderId);
   const orderDetail = orderDetails.find((detail) => detail.OrderID === orderId);
+  const order = orders.find((order) => order.OrderID === orderId);
 
   if (orderDetail) {
-    res.json(orderDetail);
+    if (order) {
+      // Combine orderDetails and orders information
+      const combinedInfo = { ...orderDetail, OrderDate: order.OrderDate, CustomerID: order.CustomerID };
+      res.json(combinedInfo);
+    } else {
+      res.status(404).json({ error: 'Order not found' });
+    }
   } else {
     res.status(404).json({ error: 'Order detail not found' });
   }
